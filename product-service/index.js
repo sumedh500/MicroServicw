797e1c777ce0d5a602f105de842db813de97cf6e
Sumedh199 <129769187+sumedh500@users.noreply.github.com>
2023-06-20 17:14:25 +0530

    packages install

---
+++ product-service/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const productRouter = require("./routes/product");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/products", productRouter);
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/scan-product-service", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Product-Service Connected to MongoDB"))
+  .catch((e) => console.log(e));
+
+app.listen(PORT, () => {
+  console.log(`Product-Service listening on port ${PORT}`);
+});
